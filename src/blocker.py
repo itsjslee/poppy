d707cf52247d63c4acf4223403692b4951d06e91
itsjslee <itsjosh343@gmail.com>
2025-01-08 23:41:42 -0500

    script created

---
+++ src/blocker.py
@@ -0,0 +1,20 @@
+# script.py
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+# Set up the WebDriver (make sure to specify the correct path to your WebDriver)
+driver = webdriver.Chrome(executable_path='path/to/chromedriver')
+
+# Function to check and modify the URL
+def check_youtube_url():
+    while True:
+        current_url = driver.current_url
+        if "youtube.com" in current_url and not current_url.endswith('.'):
+            new_url = current_url.replace("youtube.com", "youtube.com.")
+            driver.get(new_url)
+        time.sleep(5)  # Check every 5 seconds
+
+# Open YouTube and start checking the URL
+driver.get("https://www.youtube.com")
+check_youtube_url()
