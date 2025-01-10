6c3ba70ca75e015f7b13f719f806e3e90d907206
itsjslee <itsjosh343@gmail.com>
2025-01-09 23:16:59 -0500

    logic modification

--- src/blocker.py
+++ src/blocker.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import re
 
 # Set up the WebDriver (make sure to specify the correct path to your WebDriver)
 driver = webdriver.Chrome(executable_path='path/to/chromedriver')
@@ -11,7 +12,7 @@ def check_youtube_url():
     while True:
         current_url = driver.current_url
         if "youtube.com" in current_url and not current_url.endswith('.'):
-            new_url = current_url.replace("youtube.com", "youtube.com.")
+            new_url = re.sub(r'(youtube\.com)', r'\1.', current_url)
             driver.get(new_url)
         time.sleep(5)  # Check every 5 seconds
 
